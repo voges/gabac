a53b7ef04efe62fc727119a2d1022ecaf92e65f2
Jan Voges <janvoges@me.com>
2018-12-13 12:46:24 +0100

    More verbose git_release.sh

--- scripts/git_release.sh
+++ scripts/git_release.sh
@@ -9,9 +9,13 @@ if [ "$#" -ne 1 ]; then
     printf "Usage: %s version_string\\n" "$0"; exit -1
 fi
 
+printf "== Checking out 'master'\\n"
 git checkout master || exit -1
+printf "== Merging latest commit from 'develop' into 'master'\\n"
 git merge develop --squash --strategy=recursive --strategy-option=theirs || exit -1
+printf "== Creating new commit on 'master' with commit message '%s'\\n" "$1"
 git commit --message "$1" || exit -1
+printf "== Tagging the just created commit on 'master' with '%s'\\n" "$1"
 git tag "$1" || exit -1
-# git push origin master || exit -1
-# git push origin "$1" || exit -1
+printf "== If everything looks correct please manually push everything: "
+printf "git push origin master && git push origin %s\\n" "$1"
