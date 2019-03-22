f58593652d36ae113b270dd0a9ae2fb28e46a429
Fabian Müntefering <fabian.muentefering@mail.de>
2019-03-22 01:19:50 -0500

    Replaced boost/json with nlohmann/json

--- source/gabac/configuration.cpp
+++ source/gabac/configuration.cpp
@@ -6,8 +6,7 @@
 #include <string>
 #include <vector>
 
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
+#include <nlohmann/json.hpp>
 
 #include "exceptions.h"
 
@@ -26,61 +25,46 @@ EncodingConfiguration::EncodingConfiguration()
 
 
 EncodingConfiguration::EncodingConfiguration(
-        const std::string& json
+        const std::string& jsonstring
 ){
+    using namespace nlohmann;
     try {
-        // Read the stringstream JSON data to a property tree
-        std::stringstream tmp(json);
-        boost::property_tree::ptree propertyTree;
-        boost::property_tree::read_json(tmp, propertyTree);
+        auto jtree = json::parse(jsonstring);
+        this->wordSize = jtree["wordsize"];
+        this->sequenceTransformationId = jtree["sequence_transformation_id"];
+        this->sequenceTransformationParameter = jtree["sequence_transformation_parameter"];
 
-        // Convert the property tree contents to our internal structure
-        this->wordSize
-                = propertyTree.get<unsigned int>("word_size");
-        this->sequenceTransformationId
-                =
-                static_cast<gabac::SequenceTransformationId>(propertyTree.get<unsigned int>("sequence_transformation_id"));
-        this->sequenceTransformationParameter
-                = propertyTree.get<unsigned int>("sequence_transformation_parameter");
-        for (const auto& child : propertyTree.get_child("transformed_sequences")) {
+        for (const auto& child : jtree["transformed_sequences"]) {
             // Declare a transformed sequence configuration
             TransformedSequenceConfiguration transformedSequenceConfiguration;
 
             // Fill the transformed sequence configuration
-            transformedSequenceConfiguration.lutTransformationEnabled
-                    = static_cast<bool>(child.second.get<unsigned int>("lut_transformation_enabled"));
+            transformedSequenceConfiguration.lutTransformationEnabled = child["lut_transformation_enabled"];
             transformedSequenceConfiguration.lutBits = wordSize * 8;
             transformedSequenceConfiguration.lutOrder = 0;
             if (transformedSequenceConfiguration.lutTransformationEnabled) {
-                if (child.second.count("lut_transformation_bits") > 0) {
-                    transformedSequenceConfiguration.lutBits
-                            = child.second.get<unsigned int>("lut_transformation_bits");
+                if (child.count("lut_transformation_bits") > 0) {
+                    transformedSequenceConfiguration.lutBits = child["lut_transformation_bits"];
                 }
-                if (child.second.count("lut_transformation_order") > 0) {
-                    transformedSequenceConfiguration.lutOrder
-                            = child.second.get<unsigned int>("lut_transformation_order");
+                if (child.count("lut_transformation_order") > 0) {
+                    transformedSequenceConfiguration.lutOrder = child["lut_transformation_order"];
                 }
             } else {
                 transformedSequenceConfiguration.lutBits = 0;
                 transformedSequenceConfiguration.lutOrder = 0;
             }
-            transformedSequenceConfiguration.diffCodingEnabled
-                    = child.second.get<bool>("diff_coding_enabled");
-            transformedSequenceConfiguration.binarizationId
-                    = static_cast<gabac::BinarizationId>(child.second.get<unsigned int>("binarization_id"));
-            for (const auto& grandchild : child.second.get_child("binarization_parameters")) {
-                transformedSequenceConfiguration.binarizationParameters
-                        .push_back(grandchild.second.get_value<unsigned int>());
+            transformedSequenceConfiguration.diffCodingEnabled = child["diff_coding_enabled"];
+            transformedSequenceConfiguration.binarizationId = child["binarization_id"];
+            for (const auto& grandchild : child["binarization_parameters"]) {
+                transformedSequenceConfiguration.binarizationParameters.push_back(grandchild);
             }
-            transformedSequenceConfiguration.contextSelectionId
-                    = static_cast<gabac::ContextSelectionId>(child.second.get<unsigned int>("context_selection_id"));
+            transformedSequenceConfiguration.contextSelectionId = child["context_selection_id"];
 
             // Append the filled transformed sequence configuration to our
             // list of transformed sequence configurations
             this->transformedSequenceConfigurations.push_back(transformedSequenceConfiguration);
         }
-    }
-    catch (const boost::property_tree::ptree_error& e) {
+    } catch (json::exception &e) {
         GABAC_DIE("JSON parsing error: " + std::string(e.what()));
     }
 }
@@ -90,85 +74,42 @@ EncodingConfiguration::~EncodingConfiguration() = default;
 
 
 std::string EncodingConfiguration::toJsonString() const{
-    std::string jsonString;
-
+    using namespace nlohmann;
     try {
-        // Set up a property tree
-        boost::property_tree::ptree root;
+        json root;
+        root["word_size"] = this->wordSize;
+        root["sequence_transformation_id"] = this->sequenceTransformationId;
+        root["sequence_transformation_parameter"] = this->sequenceTransformationParameter;
 
-        // Convert the internal structure to a property tree
-        root.put(
-                "word_size",
-                this->wordSize
-        );
-        root.put(
-                "sequence_transformation_id",
-                static_cast<int>(this->sequenceTransformationId)
-        );
-        root.put(
-                "sequence_transformation_parameter",
-                static_cast<int>(this->sequenceTransformationParameter)
-        );
-        boost::property_tree::ptree transformedSequencesNode;
+        json sequenceList;
         for (const auto& transformedSequenceConfiguration : this->transformedSequenceConfigurations) {
-            // Declare a property tree for a transformed sequence configuration
-            boost::property_tree::ptree transformedSequenceNode;
+            json curSequence;
 
             // Fill the property tree for the transformed sequence
             // configuration
-            transformedSequenceNode.put(
-                    "lut_transformation_enabled",
-                    static_cast<int>(transformedSequenceConfiguration.lutTransformationEnabled)
-            );
+
+            curSequence["lut_transformation_enabled"] = transformedSequenceConfiguration.lutTransformationEnabled;
+
             if (transformedSequenceConfiguration.lutTransformationEnabled) {
-                transformedSequenceNode.put(
-                        "lut_transformation_bits",
-                        static_cast<int>(transformedSequenceConfiguration.lutBits)
-                );
-                transformedSequenceNode.put(
-                        "lut_transformation_order",
-                        static_cast<int>(transformedSequenceConfiguration.lutOrder)
-                );
+                curSequence["lut_transformation_bits"] = transformedSequenceConfiguration.lutBits;
+                curSequence["lut_transformation_order"] = transformedSequenceConfiguration.lutOrder;
             }
-            transformedSequenceNode.put(
-                    "diff_coding_enabled",
-                    static_cast<int>(transformedSequenceConfiguration.diffCodingEnabled)
-            );
-            transformedSequenceNode.put(
-                    "binarization_id",
-                    static_cast<int>(transformedSequenceConfiguration.binarizationId)
-            );
-            boost::property_tree::ptree binarizationParametersNode;
-            for (const auto& binarizationParameter : transformedSequenceConfiguration.binarizationParameters) {
-                boost::property_tree::ptree tmp;
-                tmp.put("", binarizationParameter);
-                binarizationParametersNode.push_back(std::make_pair("", tmp));
-            }
-            transformedSequenceNode.add_child(
-                    "binarization_parameters",
-                    binarizationParametersNode
-            );
-            transformedSequenceNode.put(
-                    "context_selection_id",
-                    static_cast<int>(transformedSequenceConfiguration.contextSelectionId)
-            );
 
-            // Add the filled property tree for the transformed sequence
-            // configuration to our tree
-            transformedSequencesNode.push_back(std::make_pair("", transformedSequenceNode));
+            curSequence["diff_coding_enabled"] = transformedSequenceConfiguration.diffCodingEnabled;
+            curSequence["binarization_id"] = transformedSequenceConfiguration.binarizationId;
+
+            curSequence["binarization_parameters"] = transformedSequenceConfiguration.binarizationParameters;
+
+            curSequence["context_selection_id"] = transformedSequenceConfiguration.contextSelectionId;
+
+            sequenceList.push_back(curSequence);
         }
-        root.push_back(std::make_pair("transformed_sequences", transformedSequencesNode));
+        root["transformed_sequences"]  = sequenceList;
+        return root.dump(4);
 
-        // Convert the property tree to a JSON string
-        std::stringstream s;
-        boost::property_tree::write_json(s, root);
-        jsonString = s.str();
+    } catch (json::exception &e) {
+        GABAC_DIE("JSON parsing error: " + std::string(e.what()));
     }
-    catch (const boost::property_tree::ptree_error& e) {
-        GABAC_DIE("JSON write error: " + std::string(e.what()));
-    }
-
-    return jsonString;
 }
 
 
